dc48baf195237a97b33432dbe737f247f494cdaf
dev <redherring2141@kaist.ac.kr>
2020-05-20 23:26:44 +0900

    Set to runnable state.

--- ac_lane_detection/processFile.py
+++ ac_lane_detection/processFile.py
@@ -12,7 +12,7 @@ from adjustThreshold import adjust_threshold
 from thresholdColorGrad import hls_wy_bin, threshold, hls_select, lab_select, luv_select, abs_sobel_th, mag_sobel_th, dir_sobel_th
 from findLane import mask_roi, mask_window, find_window_centroids, show_window_centroids, polyfit_window, measure_curve_r, get_offset
 
-file_path = "../ac_laguna_mx5_2.mp4"
+file_path = "../../ac_laguna_mx5_2.mp4"
 cap = cv2.VideoCapture(file_path)
 threshold = [0 , 68]
 kernel = 9
@@ -52,7 +52,7 @@ while(cap.isOpened()):
             kernel_dir = kernel
 
         else:
-            threshold_hls = (0,255)
+            threshold_hls = (20,70)
             #direction_hls = direction
             #kernel_hls = kernel
 
@@ -80,13 +80,13 @@ while(cap.isOpened()):
 
         #img_roi = mask_roi(img)
         img_roi = img[484:812,:]
-        #img_hls = hls_select(img_roi, ch='s', thresh=threshold_hls)
-        img_lab = cv2.cvtColor(src)
+        img_hls = hls_select(img_roi, ch='s', thresh=threshold_hls)
+        #img_lab = cv2.cvtColor(src)
         img_grad_abx = abs_sobel_th(img_roi, orient='x', ksize=kernel_abx, thresh=threshold_abx)
         img_grad_aby = abs_sobel_th(img_roi, orient='y', ksize=kernel_aby, thresh=threshold_aby)
         img_grad_mag = mag_sobel_th(img_roi, ksize=kernel_mag, thresh=threshold_mag)
         img_wy = hls_wy_bin(img_roi)
-        img_fin = img_wy
+        img_fin = img_hls
         cv2.imshow("test", img_fin*255)
 
 
