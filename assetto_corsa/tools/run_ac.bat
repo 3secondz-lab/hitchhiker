1874f4f2eb5a2bb04819d9c018b16699ccffe8d4
Jinsun <zzangjinsun@gmail.com>
2020-06-09 16:11:37 +0900

    Windows batch file minor modification

--- assetto_corsa/tools/run_ac.bat
+++ assetto_corsa/tools/run_ac.bat
@@ -2,7 +2,7 @@ set ROS_CATKIN_WS=~/ros_catkin_ws
 set ROS_WS=~/catkin_ws
 set USB_IP=/mnt/c/usbip
 
-set RUN_CMD="source %ROS_CATKIN_WS%/install/setup.bash && source %ROS_WS%/devel/setup.bash && roslaunch assetto_corsa run_ac.launch & /mnt/c/usbip/usbip.exe -a localhost "1-1" 1>nul "
+set RUN_CMD="source %ROS_CATKIN_WS%/install/setup.bash && source %ROS_WS%/devel/setup.bash && roslaunch assetto_corsa run_ac.launch & /mnt/c/usbip/usbip.exe -a localhost "1-1" 1>/dev/null"
 
 echo "Run ROS packages..."
 bash -c %RUN_CMD%
